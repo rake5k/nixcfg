7c9544bb2c0576005b31881aae70673ed531c5a3
Christian Harke <christian@harke.ch>
2022-11-30 21:44:03 +0100

    Add additional nix binary caches

--- nixos/base/nix/default.nix
+++ nixos/base/nix/default.nix
@@ -6,13 +6,23 @@
   ];
 
   nix = {
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
     gc = {
       automatic = true;
       dates = "04:00";
       options = "--delete-older-than 7d";
+    };
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [
+        "https://christianharke.cachix.org/"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+      ];
+      trusted-public-keys = [
+        "christianharke.cachix.org-1:TzmbiNLRcH8G0932XRlQzh8GPvuV9pJcHLqLnzznLKU="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
     };
   };
 
