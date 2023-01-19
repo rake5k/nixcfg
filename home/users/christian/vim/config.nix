a5da445f1066902911d30f9210b02a31ef4ae825
Christian Harke <christian@harke.ch>
2023-01-19 07:56:47 +0100

    Initial spacevim config

--- home/users/christian/vim/config.nix
+++ home/users/christian/vim/config.nix
@@ -5,29 +5,90 @@
   ];
   layers = [
     { name = "default"; }
+    # VCS
+
+    { name = "VersionControl"; }
+    { name = "git"; }
+    { name = "github"; }
+
+    # Language
+
     {
-      enable = true;
-      name = "colorscheme";
+      name = "autocomplete";
+      auto_completion_return_key_behavior = "smart";
+      auto_completion_tab_key_behavior = "smart";
+      autocomplete_method = "coc";
     }
+
+    {
+      name = "lsp";
+      filetypes = [
+        "css"
+        "docker"
+        "elm"
+        "haskell"
+        "html"
+        "java"
+        "javascript"
+        "json"
+        "nix"
+        "python"
+        "rust"
+        "sass"
+        "scss"
+        "sh"
+        "typescript"
+        "vue"
+      ];
+      override_cmd = {
+        css = [ "vscode-css-language-server" ];
+        docker = [ "docker-langserver" ];
+        elm = [ "elm-language-server" ];
+        haskell = [ "haskell-language-server-wrapper" ];
+        html = [ "vscode-html-language-server" ];
+        java = [ "jdt-language-server" ];
+        json = [ "vscode-json-language-server" ];
+        nix = [ "rnix-lsp" ];
+        python = [ "pylsp" ];
+        rust = [ "rust-analyzer" ];
+        sass = [ "vscode-css-language-server" ];
+        scss = [ "vscode-css-language-server" ];
+        vue = [ "vls" ];
+      };
+    }
+
+    { name = "lang#c"; }
+    { name = "lang#dockerfile"; }
+    { name = "lang#graphql"; }
+    { name = "lang#haskell"; }
+    { name = "lang#html"; }
+    { name = "lang#java"; }
+    { name = "lang#javascript"; }
+    { name = "lang#latex"; }
+    { name = "lang#markdown"; }
+    { name = "lang#nix"; }
+    { name = "lang#python"; }
+    { name = "lang#rust"; }
+    { name = "lang#toml"; }
+    { name = "lang#typescript"; }
+    { name = "lang#vim"; }
+    { name = "lang#vue"; }
+    { name = "lang#extra"; }
+
+    # Utilities
+
+    { name = "test"; }
     { name = "fzf"; }
     {
       default_height = 30;
       default_position = "top";
       name = "shell";
     }
-    { name = "edit"; }
-    { name = "VersionControl"; }
-    { name = "git"; }
+    { name = "sudo"; }
     {
-      auto-completion-return-key-behavior = "complete";
-      auto-completion-tab-key-behavior = "cycle";
-      autocomplete_method = "coc";
-      name = "autocomplete";
+      enable = true;
+      name = "colorscheme";
     }
-    { name = "lang#nix"; }
-    { name = "lang#java"; }
-    { name = "lang#sh"; }
-    { name = "lang#html"; }
   ];
   options = {
     buffer_index_type = 4;
