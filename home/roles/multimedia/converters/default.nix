f675c56236a90a4ae12479c57a4e6b0fb2aac9b0
Christian Harke <christian@harke.ch>
2023-03-02 21:25:30 +0100

    Add cd/dvd ripping aliases/scripts

--- home/roles/multimedia/converters/default.nix
+++ home/roles/multimedia/converters/default.nix
@@ -24,9 +24,20 @@ in
         picard
       ];
 
-      file."bin/mp3conv" = {
-        executable = true;
-        source = ./scripts/mp3conv;
+      file = {
+        "bin/mp3conv" = {
+          executable = true;
+          source = ./scripts/mp3conv;
+        };
+        "bin/ripdvd-mp4" = {
+          executable = true;
+          source = ./scripts/ripdvd-mp4;
+        };
+      };
+
+      shellAliases = {
+        ripcd-mp3 = "abcde -c ~/.config/abcde/abcde_mp3_lame.conf";
+        ripcd-flac = "abcde -c ~/.config/abcde/abcde_lossless_flac.conf";
       };
     };
   };
