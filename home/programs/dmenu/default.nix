09c1652eae7d8af533eced3a8ce4d677c0331c6c
Christian Harke <christian@harke.ch>
2023-03-24 21:44:30 +0100

    Position dmenu centered

--- home/programs/dmenu/default.nix
+++ home/programs/dmenu/default.nix
@@ -12,6 +12,10 @@ let
         url = "https://tools.suckless.org/dmenu/patches/line-height/dmenu-lineheight-5.2.diff";
         sha256 = "0jabb2ycfn3xw0k2d2rv7nyas5cwjr6zvwaffdn9jawh62c50qy5";
       }
+      {
+        url = "https://tools.suckless.org/dmenu/patches/center/dmenu-center-5.2.diff";
+        sha256 = "1jck88ypx83b73i0ys7f6mqikswgd2ab5q0dfvs327gsz11jqyws";
+      }
     ];
   };
 
