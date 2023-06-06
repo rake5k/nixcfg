e3f233d6c7ab09c69348e60a4b908d4a74018dce
Christian Harke <christian@harke.ch>
2023-06-06 08:12:19 +0200

    Update to NixOS 23.05

--- home/programs/ranger/default.nix
+++ home/programs/ranger/default.nix
@@ -48,7 +48,7 @@ in
       ];
 
       sessionVariables = {
-        RANGERCD = true;
+        RANGERCD = "true";
       };
     };
 
--- nixos/base/default.nix
+++ nixos/base/default.nix
@@ -43,9 +43,7 @@ in
 
   config = {
 
-    boot = {
-      cleanTmpDir = true;
-    };
+    boot.tmp.cleanOnBoot = true;
 
     home-manager = {
       backupFileExtension = "hm-bak";
