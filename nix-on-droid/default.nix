a4f6b7b78259496b80d6f77062ec9cf78b26a8d4
Christian Harke <christian@harke.ch>
2023-10-15 16:51:21 +0200

    Fix attribute `name` not defined error

--- nix-on-droid/default.nix
+++ nix-on-droid/default.nix
@@ -1,48 +1,67 @@
-{ config, inputs, pkgs, homeModules, name, ... }:
+{ config, lib, pkgs, homeModules, inputs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.custom.base;
+
+in
 
 {
-  environment = {
-    etcBackupExtension = ".nod-bak";
-    motd = ''
-           __     ( )               ___       __            ___   /  __      ___     ( )  ___   /
-        //   ) ) / / \\ / / ____  //   ) ) //   ) ) ____  //   ) / //  ) ) //   ) ) / / //   ) /
-       //   / / / /   \/ /       //   / / //   / /       //   / / //      //   / / / / //   / /
-      //   / / / /    / /\      ((___/ / //   / /       ((___/ / //      ((___/ / / / ((___/ /
-    '';
-
-    packages = with pkgs; [
-      diffutils
-      findutils
-      git
-      gnugrep
-      hostname
-      man
-      vim
-    ];
-  };
-
-  home-manager = {
-    backupFileExtension = "hm-bak";
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    extraSpecialArgs = { inherit inputs; };
-    sharedModules = homeModules;
-
-    config = "${inputs.self}/hosts/${name}/home-nix-on-droid.nix";
-  };
-
-  nix.package = pkgs.nix;
-
-  terminal.font =
-    let
-      fontPackage = pkgs.nerdfonts.override {
-        fonts = [ "VictorMono" ];
+  options = {
+    custom.base = {
+      hostname = mkOption {
+        type = types.str;
+        description = "Host name.";
       };
-      fontPath = "/share/fonts/truetype/NerdFonts/VictorMonoNerdFont-Regular.ttf";
-    in
-    fontPackage + fontPath;
+    };
+  };
 
-  time.timeZone = "Europe/Zurich";
+  config = {
+    environment = {
+      etcBackupExtension = ".nod-bak";
+      motd = ''
+             __     ( )               ___       __            ___   /  __      ___     ( )  ___   /
+          //   ) ) / / \\ / / ____  //   ) ) //   ) ) ____  //   ) / //  ) ) //   ) ) / / //   ) /
+         //   / / / /   \/ /       //   / / //   / /       //   / / //      //   / / / / //   / /
+        //   / / / /    / /\      ((___/ / //   / /       ((___/ / //      ((___/ / / / ((___/ /
+      '';
 
-  user.shell = "${pkgs.zsh}/bin/zsh";
+      packages = with pkgs; [
+        diffutils
+        findutils
+        git
+        gnugrep
+        hostname
+        man
+        vim
+      ];
+    };
+
+    home-manager = {
+      backupFileExtension = "hm-bak";
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      extraSpecialArgs = { inherit inputs; };
+      sharedModules = homeModules;
+
+      config = "${inputs.self}/hosts/${cfg.hostname}/home-nix-on-droid.nix";
+    };
+
+    nix.package = pkgs.nix;
+
+    terminal.font =
+      let
+        fontPackage = pkgs.nerdfonts.override {
+          fonts = [ "VictorMono" ];
+        };
+        fontPath = "/share/fonts/truetype/NerdFonts/VictorMonoNerdFont-Regular.ttf";
+      in
+      fontPackage + fontPath;
+
+    time.timeZone = "Europe/Zurich";
+
+    user.shell = "${pkgs.zsh}/bin/zsh";
+  };
 }
