0938d5604d501bdc78e2951338bb45b5bbbcddb3
Christian Harke <christian@harke.ch>
2023-10-20 11:20:05 +0200

    Simplify nix-on-droid user bin scripts definition

--- home/roles/mobile/bin/default.nix
+++ home/roles/mobile/bin/default.nix
@@ -4,19 +4,7 @@ with lib;
 
 let
 
-  inherit (config.lib.custom) genAttrs';
-
   cfg = config.custom.roles.mobile.bin;
-
-  mkUserBinScript = name:
-    {
-      name = "bin/${name}";
-      value = {
-        source = ./scripts + "/${name}";
-        target = config.home.homeDirectory + "/bin/${name}";
-        executable = true;
-      };
-    };
 
 in
 
@@ -28,10 +16,9 @@ in
   };
 
   config = mkIf cfg.enable {
-    xdg.configFile = genAttrs'
-      [
-        "termux-file-editor"
-      ]
-      mkUserBinScript;
+    home.file."bin/termux-file-editor" = {
+      source = ./scripts/termux-file-editor;
+      executable = true;
+    };
   };
 }
