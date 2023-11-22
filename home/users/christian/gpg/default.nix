b882230a8ab555fbfa9a85d184198054a41e2c65
Christian Harke <christian@harke.ch>
2023-11-22 01:19:45 +0100

    Add gpg module

---
+++ home/users/christian/gpg/default.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.custom.users.christian.gpg;
+
+in
+
+{
+  options = {
+    custom.users.christian.gpg = {
+      enable = mkEnableOption "GnuPG";
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    home.file.".gnupg/gpg-agent.conf" = {
+      text = ''
+        pinentry-program ${getExe pkgs.pinentry}
+      '';
+    };
+
+    programs.gpg.enable = true;
+  };
+}
