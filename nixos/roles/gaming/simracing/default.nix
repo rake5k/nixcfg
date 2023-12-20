24fb33a3b2be6039a0518682f4413520244daf01
Christian Harke <christian@harke.ch>
2023-12-21 00:04:28 +0100

    Add Fanatec wheel driver

---
+++ nixos/roles/gaming/simracing/default.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.custom.roles.gaming.simracing;
+
+  fanatecff = pkgs.linuxPackages.callPackage ../../../../pkgs/hid-fanatecff/default.nix { };
+
+in
+
+{
+  options = {
+    custom.roles.gaming.simracing = {
+      enable = mkEnableOption "Simracing";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    boot = {
+      extraModulePackages = [ fanatecff ];
+      kernelModules = [ "hid-fanatec" ];
+    };
+
+    services.udev.packages = [ fanatecff ];
+  };
+}
