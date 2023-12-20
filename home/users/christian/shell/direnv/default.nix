d43f51da1a753f3f6dd3517a56f39b3601910bdc
chr <christian.harke@bluecare.ch>
2023-12-20 07:32:23 +0100

    Fix direnv's `sha1sum not found` on mac

--- home/users/christian/shell/direnv/default.nix
+++ home/users/christian/shell/direnv/default.nix
@@ -17,22 +17,26 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-      stdlib = ''
-        direnv reload
+    programs.direnv =
+      let
+        coreutils = "${pkgs.coreutils}/bin";
+      in
+      {
+        enable = true;
+        nix-direnv.enable = true;
+        stdlib = ''
+          direnv reload
 
-        : ''${XDG_CACHE_HOME:=$HOME/.cache}
-        declare -A direnv_layout_dirs
-        direnv_layout_dir() {
-            echo "''${direnv_layout_dirs[$PWD]:=$(
-                echo -n "$XDG_CACHE_HOME"/direnv/layouts/
-                echo -n "$PWD" | sha1sum | cut -d ' ' -f 1
-            )}"
-        }
-      '';
-    };
+          : ''${XDG_CACHE_HOME:=$HOME/.cache}
+          declare -A direnv_layout_dirs
+          direnv_layout_dir() {
+              echo "''${direnv_layout_dirs[$PWD]:=$(
+                  echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+                  echo -n "$PWD" | ${coreutils}/sha1sum | ${coreutils}/cut -d ' ' -f 1
+              )}"
+          }
+        '';
+      };
 
     home.sessionVariables = {
       DIRENV_LOG_FORMAT = "";
