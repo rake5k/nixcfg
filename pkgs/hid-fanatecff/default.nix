6adea9404eec8d2c10ec7f7c256ebf8b60a8ca13
Christian Harke <christian@harke.ch>
2023-12-21 00:04:28 +0100

    Add Fanatec wheel driver

---
+++ pkgs/hid-fanatecff/default.nix
@@ -0,0 +1,43 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, kernel
+, kmod
+, linuxConsoleTools
+, runtimeShell
+}:
+
+let moduledir = "lib/modules/${kernel.version}/kernel/drivers/hid";
+in
+stdenv.mkDerivation rec {
+  pname = "hid-fanatecff";
+  version = "0.0.2";
+  name = "hid-fanatecff-${version}-${kernel.version}";
+
+  src = fetchFromGitHub {
+    owner = "gotzl";
+    repo = "hid-fanatecff";
+    rev = "next";
+    hash = "sha256-FZc69a2joiCi8pPxeJxhORaqa/ABRecPqDhDCc2GwL0=";
+  };
+
+  hardeningDisable = [ "pic" "format" ];
+  nativeBuildInputs = kernel.moduleBuildDependencies;
+
+  patchPhase = ''
+    mkdir -p $out/lib/udev/rules.d
+    mkdir -p $out/${moduledir}
+    substituteInPlace Makefile --replace "/etc/udev/rules.d" "$out/lib/udev/rules.d"
+    substituteInPlace fanatec.rules \
+      --replace "/bin/sh" "${runtimeShell}" \
+      --replace "/usr/bin/evdev-joystick" "${linuxConsoleTools}/bin/evdev-joystick" \
+      --replace "GROUP:=\"plugdev\"" "TAG+=\"uaccess\""
+    sed -i '/depmod/d' Makefile
+  '';
+
+  makeFlags = [
+    "KVERSION=${kernel.modDirVersion}"
+    "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "MODULEDIR=$(out)/${moduledir}"
+  ];
+}
