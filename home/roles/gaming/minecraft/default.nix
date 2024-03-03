5842a568fc5a516d17aba2ecf8cc0ffa278b3e51
Christian Harke <christian@harke.ch>
2024-03-03 11:21:53 +0100

    Add minecraft role

---
+++ home/roles/gaming/minecraft/default.nix
@@ -0,0 +1,22 @@
+{ lib, config, pkgs, ... }:
+
+let
+
+  cfg = config.custom.roles.gaming.minecraft;
+
+in
+
+{
+  options = {
+    custom.roles.gaming.minecraft = {
+      enable = lib.mkEnableOption "Minecraft";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      prismlauncher
+      jre8
+    ];
+  };
+}
