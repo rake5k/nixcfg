6a4b4605e0b7414379bd6cf5599720157ff02bb8
Christian Harke <christian@harke.ch>
2024-06-06 21:20:08 +0200

    Update home-manager nixpkgs input override to match the original channel

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     darwin = {
