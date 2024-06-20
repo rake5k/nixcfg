a660599f09652a1bea829c45ae69ec5f7c4efb43
Christian Harke <christian@harke.ch>
2024-06-20 08:58:24 +0200

    Refactor FF config

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -35,9 +35,68 @@ in
     programs.firefox = {
       inherit (cfg) enable;
 
+      policies = {
+        PasswordManagerEnabled = false;
+        DisableFirefoxAccounts = false;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+          EmailTracking = true;
+        };
+        FirefoxHome = {
+          Search = true;
+          TopSites = true;
+          SponsoredTopSites = false;
+          Highlights = false;
+          Locked = true;
+        };
+        HomePage = {
+          URL = cfg.homepage;
+          StartPage = "homepage-locked";
+          Locked = true;
+        };
+        HttpsOnlyMode = "force_enabled";
+        OfferToSaveLogins = false;
+        Preferences = {
+          "browser.newtabpage.pinned" = [
+            {
+              label = "Blog";
+              url = "https://blog.harke.ch";
+            }
+            {
+              label = "Cloud";
+              url = "https://cloud.harke.ch";
+            }
+            {
+              label = "Code";
+              url = "https://code.harke.ch";
+            }
+            {
+              label = "News";
+              url = "https://news.harke.ch";
+            }
+            {
+              label = "ProtonMail";
+              url = "https://mail.proton.me";
+            }
+          ];
+          "browser.search.suggest.enabled" = false;
+          "browser.search.region" = "CH";
+          "cookiebanners.service.mode" = 1;
+          "cookiebanners.service.mode.privateBrowsing" = 1;
+        };
+      };
+
       profiles."ztbvdcs8.default" = {
         inherit (cfg) extensions;
+
         isDefault = true;
+
         containers = {
           personal = {
             color = "turquoise";
@@ -75,90 +134,84 @@ in
             id = 6;
             name = "Danger Zone";
           };
-        };
-        search = {
-          default = "Harke Search";
-          engines = {
-            "Harke Search" = {
-              urls = [{
-                template = "https://search.harke.ch/search";
-                params = [
-                  { name = "q"; value = "{searchTerms}"; }
-                ];
-              }];
-              icon = ./icons/search.harke.ch.svg;
-            };
-
-            "Nix Packages" = {
-              urls = [{
-                template = "https://search.nixos.org/packages";
-                params = [
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
-            };
-
-            "NixOS Options" = {
-              urls = [{
-                template = "https://search.nixos.org/options";
-                params = [
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@no" ];
-            };
-
-            "NixOS Wiki" = {
-              urls = [{
-                template = "https://nixos.wiki/index.php";
-                params = [
-                  { name = "search"; value = "{searchTerms}"; }
-                ];
-              }];
-              iconUpdateURL = "https://nixos.wiki/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # every day
-              definedAliases = [ "@nw" ];
-            };
-
-            "Amazon.de".metaData.hidden = true;
-            "Bing".metaData.hidden = true;
-            "eBay".metaData.hidden = true;
-            "Google".metaData.hidden = true;
+          Facebook = {
+            color = "toolbar";
+            icon = "fence";
+            id = 7;
+            name = "Facebook";
           };
-          force = true;
         };
-        settings = {
-          "browser.search.suggest.enabled" = false;
-          "browser.search.region" = "CH";
-          "browser.startup.homepage" = cfg.homepage;
-          "browser.newtabpage.pinned" = [
-            {
-              title = "Blog";
-              url = "https://blog.harke.ch";
-            }
-            {
-              title = "Cloud";
-              url = "https://cloud.harke.ch";
-            }
-            {
-              title = "Code";
-              url = "https://code.harke.ch";
-            }
-            {
-              title = "News";
-              url = "https://news.harke.ch";
-            }
-            {
-              title = "ProtonMail";
-              url = "https://mail.proton.me";
-            }
-          ];
-          "cookiebanners.service.mode" = 1;
-          "cookiebanners.service.mode.privateBrowsing" = 1;
-        };
+        containersForce = true;
+
+        search =
+          let
+            engines = {
+              harke = "Harke Search";
+              nixPkgs = "Nix Packages";
+              nixOpts = "NixOS Options";
+              nixWiki = "NixOS Wiki";
+            };
+          in
+          {
+            default = "Harke Search";
+            engines = {
+              "${engines.harke}" = {
+                urls = [{
+                  template = "https://search.harke.ch/search";
+                  params = [
+                    { name = "q"; value = "{searchTerms}"; }
+                  ];
+                }];
+                icon = ./icons/search.harke.ch.svg;
+              };
+
+              "${engines.nixPkgs}" = {
+                urls = [{
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }];
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@np" ];
+              };
+
+              "${engines.nixOpts}" = {
+                urls = [{
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }];
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@no" ];
+              };
+
+              "${engines.nixWiki}" = {
+                urls = [{
+                  template = "https://nixos.wiki/index.php";
+                  params = [
+                    { name = "search"; value = "{searchTerms}"; }
+                  ];
+                }];
+                iconUpdateURL = "https://nixos.wiki/favicon.png";
+                updateInterval = 24 * 60 * 60 * 1000; # every day
+                definedAliases = [ "@nw" ];
+              };
+
+              "Amazon.de".metaData.hidden = true;
+              "Bing".metaData.hidden = true;
+              "eBay".metaData.hidden = true;
+              "Google".metaData.hidden = true;
+            };
+            force = true;
+            order = with engines; [
+              harke
+              nixPkgs
+              nixOpts
+              nixWiki
+            ];
+          };
       };
     };
   };
