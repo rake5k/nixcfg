22177353aa48e527c952e3dbf8892865821fdc34
Christian Harke <christian@harke.ch>
2024-06-27 22:32:19 +0200

    Enable ssh-agent

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -33,9 +33,12 @@ in
     homeage.file = listToAttrs (map mkHomeageFile cfg.identities);
     programs.ssh = {
       enable = true;
+      addKeysToAgent = "yes";
       controlMaster = "auto";
       controlPath = "~/.ssh/master-%r@%n:%p";
       controlPersist = "10m";
     };
+
+    services.ssh-agent.enable = true;
   };
 }
