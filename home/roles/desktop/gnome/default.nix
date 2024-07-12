7c345d65a22e9c5eb6ebfc8aeb16b16a22483943
Christian Harke <christian@harke.ch>
2024-07-12 12:53:56 +0200

    Configure default terminal emulator

--- home/roles/desktop/gnome/default.nix
+++ home/roles/desktop/gnome/default.nix
@@ -4,7 +4,8 @@ with lib;
 
 let
 
-  cfg = config.custom.roles.desktop.gnome;
+  desktopCfg = config.custom.roles.desktop;
+  cfg = desktopCfg.gnome;
 
 in
 
@@ -17,6 +18,11 @@ in
 
   config = mkIf cfg.enable {
     dconf.settings = with lib.hm.gvariant; {
+      "org/gnome/desktop/applications/terminal" = {
+        exec = desktopCfg.terminal.spawnCmd;
+        exec-arg = "";
+      };
+
       "org/gnome/desktop/input-sources" = {
         show-all-sources = true;
         sources = [ (mkTuple [ "xkb" "de+neo_qwertz" ]) (mkTuple [ "xkb" "de+bone" ]) ];
