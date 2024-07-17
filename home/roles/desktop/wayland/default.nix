c2abd2cf68c620dbc1ffa1645cb585e78ae54cf6
Christian Harke <christian@harke.ch>
2024-07-17 20:39:56 +0200

    Add wl-clipboard

---
+++ home/roles/desktop/wayland/default.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.custom.roles.desktop.wayland;
+
+in
+
+{
+  options = {
+    custom.roles.desktop.wayland = {
+      enable = mkEnableOption "Wayland config";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      wl-clipboard
+    ];
+  };
+}
