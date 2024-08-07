5316f4093b5871cd5546a749a2a23b5da72eb1e4
Christian Harke <christian@harke.ch>
2024-08-07 23:09:01 +0200

    Automatically enable syncthing when freetube is installed

---
+++ home/roles/web/freetube/default.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.custom.roles.web.freetube;
+
+in
+
+{
+  options = {
+    custom.roles.web.freetube = {
+      enable = mkEnableOption "FreeTube";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    custom = {
+      programs.syncthing.enable = true;
+    };
+
+    home.packages = with pkgs; [
+      freetube
+    ];
+  };
+}
