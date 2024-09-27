17d14ee512f55bbf8f0c4d2fde0690647e378b29
Christian Harke <christian@harke.ch>
2024-09-27 10:46:35 +0200

    Reuse nixpkgs-config on non-NixOS

--- home/base/nix/default.nix
+++ home/base/nix/default.nix
@@ -5,9 +5,6 @@
     pkgs.nix-output-monitor
   ];
 
-  nixpkgs.config = import ./nixpkgs-config.nix;
-  xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
-
   # Command-not-found replacement
   programs.nix-index.enable = true;
 }
