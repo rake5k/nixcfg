45df861ea9a52b2503e04bb0804ea9c6e9b7ef4d
Christian Harke <christian@harke.ch>
2024-11-09 20:12:14 +0100

    Configure nvim onedark theme

--- home/users/christian/vim/default.nix
+++ home/users/christian/vim/default.nix
@@ -115,8 +115,39 @@ in
         set thesaurus+=~/.local/share/nvim/site/dict/mthesaur.txt
       '';
 
+      extraLuaConfig = ''
+        -------------------
+        -- ONEDARK THEME --
+        -------------------
+
+        require("onedarkpro").setup({
+          styles = {
+            types = "NONE",
+            methods = "NONE",
+            numbers = "NONE",
+            strings = "NONE",
+            comments = "italic",
+            keywords = "bold,italic",
+            constants = "NONE",
+            functions = "italic",
+            operators = "NONE",
+            variables = "NONE",
+            parameters = "NONE",
+            conditionals = "italic",
+            virtual_text = "NONE",
+          },
+          options = {
+            cursorline = true,
+            transparency = true
+          }
+        })
+
+        vim.cmd("colorscheme onedark")
+      '';
+
       plugins = with pkgs.vimPlugins; [
         vim-nix
+        onedarkpro-nvim
 
         # Markdown
         tabular
