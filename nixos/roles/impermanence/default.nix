9885969103ddbb596c65d195a8e1761c5918c146
Christian Harke <christian@harke.ch>
2025-01-10 23:40:05 +0100

    Fix initrd ssh key not found

--- nixos/roles/impermanence/default.nix
+++ nixos/roles/impermanence/default.nix
@@ -92,6 +92,8 @@ in
         "/etc/ssh/ssh_host_ed25519_key.pub"
         "/etc/ssh/ssh_host_rsa_key"
         "/etc/ssh/ssh_host_rsa_key.pub"
+        "/etc/secrets/initrd/ssh_host_ed25519_key"
+        "/etc/secrets/initrd/ssh_host_ed25519_key.pub"
       ];
     };
   };
