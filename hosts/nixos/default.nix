4e5813c1ff9907b006f8da139a1366983c23f73f
Christian Harke <christian@harke.ch>
2025-01-13 22:04:01 +0100

    Add nas config from downstream flake

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -3,18 +3,19 @@
 
   custom = {
     base = {
-      system.boot.secureBoot = true;
+      system.network.wol.interface = "enp4s0";
       users = [
         "demo"
         "christian"
       ];
     };
+
     roles = {
       android.enable = true;
       containers.enable = true;
       desktop.enable = true;
       gaming.enable = true;
-      impermanence.enable = true;
+      nas.enable = true;
       printing.enable = true;
     };
   };
