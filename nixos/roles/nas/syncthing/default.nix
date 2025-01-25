48401c7a3131ea62b4f0285073686a4b2b7f4303
Christian Harke <christian@harke.ch>
2025-01-25 23:17:33 +0100

    Put syncthing web gui behind traefik

--- nixos/roles/nas/syncthing/default.nix
+++ nixos/roles/nas/syncthing/default.nix
@@ -6,6 +6,8 @@ let
 
   cfg = config.custom.roles.nas.syncthing;
 
+  guiAddress = "localhost:8384";
+
 in
 
 {
@@ -16,56 +18,83 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.syncthing = {
-      enable = true;
-      dataDir = "/var/lib/syncthing";
-      guiAddress = "0.0.0.0:8384";
-      openDefaultPorts = true;
-      settings = {
-        devices = {
-          altair = {
-            addresses = [ "tcp://172.16.4.1:22000" ];
-            id = "BOSJDVM-QMEQTKP-JSKLGT5-WLIBLRY-QFVJOKO-LMR3XNT-YXJA7OD-7LEJ3AA";
-          };
-          malmok = {
-            addresses = [ "tcp://10.0.10.2:22000" ];
-            id = "JJXAPCO-MOJLFVL-3YHBIXK-YUGQWXF-SWEGZTH-7DW2XAS-GDSQ3KC-RUQVBAB";
-          };
-          pc-win10-chr = {
-            addresses = [ "tcp://172.16.4.1:22000" ];
-            id = "CU6527N-PEXYFOV-YZSY5AI-UDRMM46-FM3BKUU-X3DSU4Y-JLEJ2YO-2CZBJAV";
-          };
-          pixel-7a = {
-            addresses = [ "tcp://10.0.10.7:22000" ];
-            id = "XKSHGGN-JSMIPZ3-MUDZHEM-LH2I6KZ-B3WTV3X-R2ZLCBI-QHPJTFR-ELYCIQN";
-          };
-          sirius-a = {
-            addresses = [ ];
-            id = "Z5CMWGO-QVYSG5A-ERDY57R-CZSI56V-6ZRRQ6U-WPA37ZL-DCT3MSZ-57MQWAW";
-          };
-          sv-syno-01 = {
-            addresses = [ "tcp://172.16.2.3:22000" ];
-            id = "EEEGHSK-UVNRNLA-2TFEOEV-RHMALFK-T4IT65L-TIJB5EA-25UXOGH-J4ZURQ7";
-          };
-        };
+    services = {
+      syncthing = {
+        inherit guiAddress;
 
-        folders = {
-          FreeTube = {
-            enable = true;
-            devices = [
-              config.services.syncthing.settings.devices.altair.name
-              config.services.syncthing.settings.devices.pc-win10-chr.name
-              config.services.syncthing.settings.devices.malmok.name
-              config.services.syncthing.settings.devices.pixel-7a.name
-            ];
-            path = "/data/syncthing/FreeTube";
+        enable = true;
+        dataDir = "/var/lib/syncthing";
+        openDefaultPorts = true;
+
+        settings = {
+          devices = {
+            altair = {
+              addresses = [ "tcp://172.16.4.1:22000" ];
+              id = "BOSJDVM-QMEQTKP-JSKLGT5-WLIBLRY-QFVJOKO-LMR3XNT-YXJA7OD-7LEJ3AA";
+            };
+            malmok = {
+              addresses = [ "tcp://10.0.10.2:22000" ];
+              id = "JJXAPCO-MOJLFVL-3YHBIXK-YUGQWXF-SWEGZTH-7DW2XAS-GDSQ3KC-RUQVBAB";
+            };
+            pc-win10-chr = {
+              addresses = [ "tcp://172.16.4.1:22000" ];
+              id = "CU6527N-PEXYFOV-YZSY5AI-UDRMM46-FM3BKUU-X3DSU4Y-JLEJ2YO-2CZBJAV";
+            };
+            pixel-7a = {
+              addresses = [ "tcp://10.0.10.7:22000" ];
+              id = "XKSHGGN-JSMIPZ3-MUDZHEM-LH2I6KZ-B3WTV3X-R2ZLCBI-QHPJTFR-ELYCIQN";
+            };
+            sirius-a = {
+              addresses = [ ];
+              id = "Z5CMWGO-QVYSG5A-ERDY57R-CZSI56V-6ZRRQ6U-WPA37ZL-DCT3MSZ-57MQWAW";
+            };
+            sv-syno-01 = {
+              addresses = [ "tcp://172.16.2.3:22000" ];
+              id = "EEEGHSK-UVNRNLA-2TFEOEV-RHMALFK-T4IT65L-TIJB5EA-25UXOGH-J4ZURQ7";
+            };
           };
-          RetroDeck = {
-            enable = true;
-            devices = [
-              config.services.syncthing.settings.devices.sirius-a.name
-            ];
-            path = "/data/syncthing/RetroDeck";
+
+          folders = {
+            FreeTube = {
+              enable = true;
+              devices = [
+                config.services.syncthing.settings.devices.altair.name
+                config.services.syncthing.settings.devices.pc-win10-chr.name
+                config.services.syncthing.settings.devices.malmok.name
+                config.services.syncthing.settings.devices.pixel-7a.name
+              ];
+              path = "/data/syncthing/FreeTube";
+            };
+            RetroDeck = {
+              enable = true;
+              devices = [
+                config.services.syncthing.settings.devices.sirius-a.name
+              ];
+              path = "/data/syncthing/RetroDeck";
+            };
+          };
+
+          # Prevent "Host check error"
+          # https://docs.syncthing.net/users/faq.html#why-do-i-get-host-check-error-in-the-gui-api
+          gui.insecureSkipHostcheck = true;
+        };
+      };
+
+      traefik = {
+        dynamicConfigOptions = {
+          http = {
+            services = {
+              syncthing.loadBalancer.servers = [ { url = "http://${guiAddress}"; } ];
+            };
+
+            routers = {
+              syncthing = {
+                entryPoints = [ "websecure" ];
+                rule = "Host(`syncthing.local.harke.ch`)";
+                service = "syncthing";
+                tls.certResolver = "letsencrypt";
+              };
+            };
           };
         };
       };
