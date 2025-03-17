407fd0b80e5eb95cc68ee3a4c76f3d7063bf9bb5
Christian Harke <christian@harke.ch>
2025-03-17 13:38:25 +0100

    Move SSH ids listing into sub-flakes

--- home/users/christian/ssh/default.nix
+++ home/users/christian/ssh/default.nix
@@ -16,14 +16,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    custom.roles.ssh = {
-      enable = true;
-      identities = [
-        "id_rsa"
-        "id_rsa.pub"
-        "id_ed25519"
-        "id_ed25519.pub"
-      ];
-    };
+    custom.roles.ssh.enable = true;
   };
 }
