e8441eb70399cf34e3da6a552242530c4e5b85c8
Christian Harke <christian@harke.ch>
2025-04-06 15:18:52 +0200

    Enable borg backup

--- nixos/roles/nas/backup/default.nix
+++ nixos/roles/nas/backup/default.nix
@@ -9,12 +9,16 @@ let
 
   cfg = config.custom.roles.nas.backup;
 
-  inherit (lib) mkEnableOption mkForce mkIf;
+  inherit (lib)
+    getExe
+    mkEnableOption
+    mkIf
+    ;
 
   inherit (config.custom.base.system.btrfs.btrbk) snapshotDir;
   inherit (config.custom.base) hostname;
 
-  btrbkId = "id_ed25519_btrbk";
+  borgId = "id_ed25519_borg";
 
 in
 
@@ -27,13 +31,7 @@ in
 
   config = mkIf cfg.enable {
 
-    # Allow btrbk user to read ssh key file
-    age.secrets."${btrbkId}" = {
-      owner = mkForce "btrbk";
-      mode = mkForce "400";
-    };
-
-    custom.base.agenix.secrets = [ btrbkId ];
+    custom.base.agenix.secrets = [ borgId ];
 
     # Make sure a USB disk is available as `/dev/disk/by-label/btrbkusbn`
     # see: https://wiki.nixos.org/wiki/Full_Disk_Encryption#Unlocking_secondary_drives
@@ -46,71 +44,96 @@ in
       ];
     };
 
-    services.btrbk.instances = {
-      # Remote root state backup to SSH
-      root-remote = {
-        onCalendar = "hourly";
+    services = {
+      borgmatic = {
+        enable = true;
         settings = {
-          snapshot_preserve = "7d 4w 6m 1y";
-          snapshot_preserve_min = "2d";
-          snapshot_dir = snapshotDir;
-
-          target_preserve = "20d 10w 6m";
-          target_preserve_min = "no";
-          target = "ssh://sv-syno-01.home.local/volume1/btrbk/${hostname}/root";
-
-          ssh_identity = config.age.secrets."${btrbkId}".path;
-          ssh_user = "btrbk";
-
-          # Synology Hack (https://github.com/digint/btrbk/issues/383#issuecomment-823808283)
-          compat = "ignore_receive_errors";
-
-          volume."/" = {
-            subvolume = {
-              home = { };
-              persist = { };
-            };
-          };
+          exclude_patterns = [
+            "/home/*/.cache"
+            "*/.vim*.tmp"
+          ];
+          exclude_if_present = [ ".nobackup" ];
+          keep_daily = 7;
+          keep_weekly = 4;
+          keep_monthly = 6;
+          keep_yearly = 1;
+          remote_path = "/usr/local/bin/borg";
+          ssh_command = "${getExe pkgs.openssh} -i ${config.age.secrets.${borgId}.path}";
+          unknown_unencrypted_repo_access_is_ok = true;
+          source_directories = [
+            "/home"
+            "/persist"
+            "/data/container"
+            "/data/home"
+            "/data/library"
+            "/data/photo"
+            "/data/share"
+            "/data/syncthing"
+          ];
+          repositories = [
+            {
+              label = "syno";
+              path = "ssh://borg@sv-syno-01.home.local/volume1/borg/hyperion";
+            }
+          ];
         };
       };
 
-      # Local data snapshots only
-      # IMPORTANT: to be backed up to a remote location otherwise
-      data-local = {
-        onCalendar = "hourly";
-        settings = {
-          snapshot_preserve = "7d 4w 6m";
-          snapshot_preserve_min = "2d";
-          snapshot_dir = "/data${snapshotDir}";
+      btrbk.instances = {
+        # Remote root state backup to SSH
+        root-remote = {
+          onCalendar = "hourly";
+          settings = {
+            snapshot_preserve = "7d 4w 6m 1y";
+            snapshot_preserve_min = "2d";
+            snapshot_dir = snapshotDir;
 
-          volume."/data" = {
-            subvolume = {
-              "container" = { };
-              "home" = { };
-              "library" = { };
-              "photo" = { };
-              "share" = { };
-              "syncthing" = { };
+            volume."/" = {
+              subvolume = {
+                home = { };
+                persist = { };
+              };
             };
           };
         };
-      };
 
-      # Local backup to external disk
-      data-external = {
-        onCalendar = "hourly";
-        settings = {
-          snapshot_preserve = "7d 4w 6m";
-          snapshot_preserve_min = "2d";
-          snapshot_dir = "/data${snapshotDir}";
+        # Local data snapshots only
+        data-local = {
+          onCalendar = "hourly";
+          settings = {
+            snapshot_preserve = "7d 4w 6m";
+            snapshot_preserve_min = "2d";
+            snapshot_dir = "/data${snapshotDir}";
 
-          target_preserve = "7d";
-          target_preserve_min = "no";
+            volume."/data" = {
+              subvolume = {
+                "container" = { };
+                "home" = { };
+                "library" = { };
+                "photo" = { };
+                "share" = { };
+                "syncthing" = { };
+              };
+            };
+          };
+        };
 
-          volume."/data" = {
-            target = "/mnt/btrbkusb/${hostname}";
-            subvolume = {
-              "plex" = { };
+        # Local backup to external disk
+        data-external = {
+          onCalendar = "hourly";
+          settings = {
+            snapshot_preserve = "7d 4w 6m";
+            snapshot_preserve_min = "2d";
+            snapshot_dir = "/data${snapshotDir}";
+
+            target_preserve = "7d";
+            target_preserve_min = "no";
+
+            volume."/data" = {
+              target = "/mnt/btrbkusb/${hostname}";
+              subvolume = {
+                "plex" = { };
+              };
             };
           };
         };
@@ -122,20 +145,5 @@ in
       "d ${snapshotDir} 0755 root root"
       "d /data${snapshotDir} 0755 root root"
     ];
-
-    users = {
-      users."backup-synology" = {
-        description = "Synology Active Backup for Business";
-        group = "backup";
-        isSystemUser = true;
-        uid = 887;
-        packages = with pkgs; [ rsync ];
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiLHdmlF5pcDII9bOSyTRA+S8OhnSZRk8VFHl42Nnct"
-        ];
-        useDefaultShell = true;
-      };
-      groups.backup.gid = 888;
-    };
   };
 }
