5e0fb041ff0d3afeb0db7bd5832bf29a623fffe6
Christian Harke <christian@harke.ch>
2025-04-07 22:53:19 +0200

    Exclude ollama models from backup

--- nixos/roles/ai/text/default.nix
+++ nixos/roles/ai/text/default.nix
@@ -16,12 +16,19 @@ in
   };
 
   config = mkIf cfg.enable {
-    custom.base.system.btrfs.impermanence.extraDirectories = [
-      # When using `DynamicUser` in sytemd services, we can't persist the actual state directory.
-      # See: https://github.com/nix-community/impermanence/issues/93
-      "/var/lib/private/ollama"
-      "/var/lib/private/open-webui"
-    ];
+    custom = {
+      base.system.btrfs.impermanence.extraDirectories = [
+        # When using `DynamicUser` in sytemd services, we can't persist the actual state directory.
+        # See: https://github.com/nix-community/impermanence/issues/93
+        "/var/lib/private/ollama"
+        "/var/lib/private/open-webui"
+      ];
+
+      roles.backup.rsync.jobs.backup.excludes = [
+        "/persist/var/lib/private/ollama/models"
+        "/var/lib/private/ollama/models"
+      ];
+    };
 
     services = {
       ollama = {
