0e437a147b46c6eec35c16c1ac190181286e41ff
Christian Harke <christian@harke.ch>
2025-04-30 21:57:47 +0200

    Enable immich photo gallery on NAS

---
+++ nixos/roles/nas/photos/default.nix
@@ -0,0 +1,74 @@
+{ config, lib, ... }:
+
+let
+
+  cfg = config.custom.roles.nas.photos;
+
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
+
+in
+
+{
+  options = {
+    custom.roles.nas.photos = {
+      enable = mkEnableOption "Photo gallery";
+
+      host = mkOption {
+        type = types.str;
+        default = "photos.local.harke.ch";
+        description = "Host name where the photo gallery is available on";
+      };
+
+      mediaPath = mkOption {
+        type = types.path;
+        default = "/data/photo";
+        description = "Path where the photo gallery data lies";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    custom.base.system.btrfs.impermanence.extraDirectories = [
+      "/var/lib/${config.services.postgresql.dataDir}"
+    ];
+
+    services = {
+      immich = {
+        enable = true;
+        openFirewall = true;
+        mediaLocation = cfg.mediaPath;
+        settings.server.externalDomain = "https://${cfg.host}";
+      };
+
+      traefik = {
+        dynamicConfigOptions = {
+          http = {
+            services = {
+              photos.loadBalancer.servers = [
+                { url = "http://localhost:${toString config.services.immich.port}"; }
+              ];
+            };
+
+            routers = {
+              photos = {
+                entryPoints = [ "websecure" ];
+                rule = "Host(`${cfg.host}`)";
+                service = "photos";
+                tls.certResolver = "letsencrypt";
+              };
+            };
+          };
+        };
+      };
+    };
+
+    systemd.tmpfiles.rules = [
+      "d ${cfg.mediaPath} 0755 ${config.services.immich.user} ${config.services.immich.group} -"
+    ];
+  };
+}
