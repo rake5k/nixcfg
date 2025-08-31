b65de94d375850e547165d85a64856f401bcf6c8
Christian Harke <christian@harke.ch>
2025-08-31 11:08:22 +0200

    Fix evaluation failure caused by stylix

--- nix-on-droid/base/default.nix
+++ nix-on-droid/base/default.nix
@@ -6,4 +6,9 @@
   options.lib = lib.mkOption {
     type = with lib.types; attrsOf attrs;
   };
+
+  config = {
+    # See: https://github.com/nix-community/stylix/issues/1818
+    stylix.overlays.enable = false;
+  };
 }
