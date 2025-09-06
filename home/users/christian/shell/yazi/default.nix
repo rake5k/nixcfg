2afd8b260be2423be033f3a6dcbb9d5df60eac04
Christian Harke <christian@harke.ch>
2025-09-06 22:34:36 +0200

    Let stylix manage yazi appearance

--- home/users/christian/shell/yazi/default.nix
+++ home/users/christian/shell/yazi/default.nix
@@ -21,61 +21,23 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.yazi =
-      let
-        gruvbox-dark = pkgs.yaziPlugins.mkYaziPlugin {
-          pname = "gruvbox-dark.yazi";
-          version = "0-unstable-2025-08-18";
-          src = pkgs.fetchFromGitHub {
-            owner = "bennyyip";
-            repo = "gruvbox-dark.yazi";
-            rev = "e5e1aefbfb5641b487cb4a11ccbc57346ec8e130";
-            hash = "sha256-NeePBNhMVXyIrED4Iu4ZSHwwgsd3CV8oBzYoQOWsD/U=";
-          };
-          meta = {
-            description = "gruvbox-dark flavor";
-            homepage = "https://github.com/bennyyip/gruvbox-dark.yazi";
-            license = lib.licenses.mit;
-            maintainers = with lib.maintainers; [ rake5k ];
-          };
-        };
+    programs.yazi = {
+      enable = true;
 
-        nord = pkgs.yaziPlugins.mkYaziPlugin {
-          pname = "nord.yazi";
-          version = "0-unstable-2025-06-03";
-          src = pkgs.fetchFromGitHub {
-            owner = "AdithyanA2005";
-            repo = "nord.yazi";
-            rev = "3a791e9197a3d3ce7003d46ab6712bfc8fef666b";
-            hash = "sha256-ubPIPasm1KHVX95nRvt03I1aLDh1qUtGL8sOHGJeZrU=";
-          };
-          meta = {
-            description = "nord flavor";
-            homepage = "https://github.com/AdithyanA2005/nord.yazi";
-            license = lib.licenses.mit;
-            maintainers = with lib.maintainers; [ rake5k ];
-          };
-        };
-      in
-      {
-        enable = true;
+      plugins = with pkgs.yaziPlugins; {
+        inherit full-border;
+      };
 
-        plugins = with pkgs.yaziPlugins; {
-          inherit full-border;
-        };
-
-        flavors = {
-          inherit gruvbox-dark nord;
-        };
-
-        initLua = ''
+      initLua = # lua
+        ''
           require("full-border"):setup {
             type = ui.Border.ROUNDED,
           }
         '';
 
-        keymap = {
-          mgr.prepend_keymap = [
+      keymap = {
+        mgr = {
+          prepend_keymap = [
             {
               run = "cd /mnt";
               on = [
@@ -85,17 +47,13 @@ in
             }
           ];
         };
+      };
 
-        settings = {
-          mgr = {
-            linemode = "size";
-          };
-        };
-
-        theme.flavor = {
-          dark = "nord";
-          light = "nord";
+      settings = {
+        mgr = {
+          linemode = "size";
         };
       };
+    };
   };
 }
