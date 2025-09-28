dbed60e022cf2e719dda90e8de03fb3b4c44b410
Christian Harke <christian@harke.ch>
2025-09-28 20:17:55 +0200

    Set system keyboard layout to ch by default

--- nixos/base/input/default.nix
+++ nixos/base/input/default.nix
@@ -1,8 +1,8 @@
 {
   services = {
     xserver.xkb = {
-      layout = "de,de";
-      variant = "neo_qwertz,bone";
+      layout = "ch,de,de";
+      variant = ",neo_qwertz,bone";
       options = "grp:rctrl_toggle,grp_led:scroll";
     };
   };
