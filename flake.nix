911fd23870d2a672e4a035fb7997ae9e367ce46b
Christian Harke <christian@harke.ch>
2025-11-14 14:10:19 +0100

    Update lanzaboote to 0.4.3

--- flake.nix
+++ flake.nix
@@ -96,7 +96,7 @@
     impermanence.url = "github:nix-community/impermanence";
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote/v0.4.3";
       inputs = {
         nixpkgs.follows = "nixpkgs-unstable";
         flake-compat.follows = "flake-compat";
