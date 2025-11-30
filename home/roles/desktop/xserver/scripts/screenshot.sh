b67343c6c21e7796cca8f35429683822695856fb
Christian Harke <christian.harke@bluecare.ch>
2025-11-30 15:35:19 +0100

    Put screenshots into the clipboard as well

--- home/roles/desktop/xserver/scripts/screenshot.sh
+++ home/roles/desktop/xserver/scripts/screenshot.sh
@@ -10,6 +10,13 @@ notify_failure() {
   notify-send -u critical "Taking screenshot failed" "${1}"
 }
 
+copy_to_clipboard_and_notify() {
+  local file="${1}"
+  local message="${2}"
+  xclip -selection clipboard -t image/png < "${file}"
+  notify_success "${message}" "${file}"
+}
+
 screenshot() {
   OUTDIR="${HOME}/Pictures/Screenshots"
   OUT="${OUTDIR}/Screenshot from %Y-%m-%d %H-%M-%S.png"
@@ -22,7 +29,7 @@ screenshot() {
     # shellcheck disable=SC2016
     FILE="$(${CMD} -m --exec 'echo $f' "${OUT}")"
     if [[ -n "${FILE}" ]]; then
-      notify_success "Fullscreen screenshot saved" "${FILE}"
+      copy_to_clipboard_and_notify "${FILE}" "Fullscreen screenshot saved"
     else
       notify_failure "Fullscreen screenshot failed"
     fi
@@ -32,7 +39,7 @@ screenshot() {
     # shellcheck disable=SC2016
     FILE="$(${CMD} -fs --line style=dash,width=2,color="yellow" --exec 'echo $f' "${OUT}")"
     if [[ -n "${FILE}" ]]; then
-      notify_success "Selection screenshot saved" "${FILE}"
+      copy_to_clipboard_and_notify "${FILE}" "Selection screenshot saved"
     else
       notify_failure "Selection screenshot aborted"
     fi
@@ -41,7 +48,7 @@ screenshot() {
     # shellcheck disable=SC2016
     FILE="$(${CMD} -u --exec 'echo $f' "${OUT}")"
     if [[ -n "${FILE}" ]]; then
-      notify_success "Window screenshot saved" "${FILE}"
+      copy_to_clipboard_and_notify "${FILE}" "Window screenshot saved"
     else
       notify_failure "Window screenshot failed"
     fi
