f87f05a801b86894c05b348bd666f91422c69f34
Christian Harke <christian@harke.ch>
2025-12-08 22:38:51 +0100

    Force overwriting gtk2 rc file

--- home/roles/desktop/gtk/default.nix
+++ home/roles/desktop/gtk/default.nix
@@ -27,18 +27,21 @@ in
         package = pkgs.paper-icon-theme;
         name = "Paper";
       };
-      gtk2.extraConfig = ''
-        gtk-toolbar-style=GTK_TOOLBAR_BOTH
-        gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-        gtk-button-images=1
-        gtk-menu-images=1
-        gtk-enable-event-sounds=1
-        gtk-enable-input-feedback-sounds=1
-        gtk-xft-antialias=1
-        gtk-xft-hinting=1
-        gtk-xft-hintstyle=hintfull
-        gtk-xft-rgba=rgb
-      '';
+      gtk2 = {
+        extraConfig = ''
+          gtk-toolbar-style=GTK_TOOLBAR_BOTH
+          gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
+          gtk-button-images=1
+          gtk-menu-images=1
+          gtk-enable-event-sounds=1
+          gtk-enable-input-feedback-sounds=1
+          gtk-xft-antialias=1
+          gtk-xft-hinting=1
+          gtk-xft-hintstyle=hintfull
+          gtk-xft-rgba=rgb
+        '';
+        force = true;
+      };
       gtk3.extraConfig = {
         gtk-application-prefer-dark-theme = 1;
         gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
