747e087495d271871bb5663f94e21ed66adaa814
Christian Harke <christian@harke.ch>
2025-12-16 23:03:25 +0100

    Fix setting the LANG env var in cinnamon

--- home/base/default.nix
+++ home/base/default.nix
@@ -1,19 +1,32 @@
 { config, lib, ... }:
 
+let
+
+  sessionVariables = {
+    LANG = "${config.home.language.base}";
+  };
+
+in
+
 {
   home = {
-    homeDirectory = lib.mkDefault "/home/${config.home.username}";
-
-    sessionPath = [ "$HOME/bin" ];
-    sessionVariables = {
-      LANG = "${config.home.language.base}";
-    };
+    inherit sessionVariables;
 
     enableNixpkgsReleaseCheck = true;
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    sessionPath = [ "$HOME/bin" ];
   };
 
   news.display = "silent";
 
+  pam = {
+    inherit sessionVariables;
+  };
+
   # See: https://github.com/nix-community/stylix/issues/1832
   stylix.overlays.enable = false;
+
+  systemd.user = {
+    inherit sessionVariables;
+  };
 }
