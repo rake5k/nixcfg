6fcde0f8db475f9dbab4b4ea1066e2a76ef6c942
Christian Harke <christian@harke.ch>
2025-12-26 21:30:21 +0100

    Add opencode

---
+++ home/roles/dev/opencode/default.nix
@@ -0,0 +1,37 @@
+{ config, lib, ... }:
+
+let
+
+  cfg = config.custom.roles.dev.opencode;
+
+  inherit (lib) mkEnableOption mkIf;
+
+in
+
+{
+  options = {
+    custom.roles.dev.opencode = {
+      enable = mkEnableOption "opencode";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.opencode = {
+      enable = true;
+      settings = {
+        provider = {
+          hyperion = {
+            npm = "@ai-sdk/openai-compatible";
+            name = "Ollama (hyperion)";
+            options.baseURL = "https://ollama.local.harke.ch/v1";
+            models = {
+              "deepseek-r1:7b".name = "Deepseek r1 7b";
+              "deepseek-r1:8b".name = "Deepseek r1 8b";
+              "gpt-oss:20b".name = "GPT OSS 20b";
+            };
+          };
+        };
+      };
+    };
+  };
+}
