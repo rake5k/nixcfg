90cc7cfa03670b5136de6ab8fbdcad893250a2d7
Christian Harke <christian@harke.ch>
2026-01-10 16:38:28 +0100

    Implement some docs-suggested yazi-configs

--- home/users/christian/shell/yazi/default.nix
+++ home/users/christian/shell/yazi/default.nix
@@ -21,6 +21,10 @@ in
   };
 
   config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      exiftool
+    ];
+
     programs.yazi = {
       enable = true;
 
@@ -33,20 +37,48 @@ in
           require("full-border"):setup {
             type = ui.Border.ROUNDED,
           }
+
+          -- Show username and hostname in header
+          Status:children_add(function()
+            local h = cx.active.current.hovered
+            if not h or ya.target_family() ~= "unix" then
+              return ""
+            end
+
+            return ui.Line {
+              ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+              ":",
+              ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+              " ",
+            }
+          end, 500, Status.RIGHT)
+
+          -- Show username and hostname in header
+          Header:children_add(function()
+             if ya.target_family() ~= "unix" then
+               return ""
+             end
+             return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+          end, 500, Header.LEFT)
         '';
 
       keymap = {
-        mgr = {
-          prepend_keymap = [
-            {
-              run = "cd /mnt";
-              on = [
-                "g"
-                "M"
-              ];
-            }
-          ];
-        };
+        input.prepend_keymap = [
+          {
+            desc = "Cancel input";
+            on = [ "<Esc>" ];
+            run = "close";
+          }
+        ];
+        mgr.prepend_keymap = [
+          {
+            on = [
+              "g"
+              "M"
+            ];
+            run = "cd /mnt";
+          }
+        ];
       };
 
       settings = {
