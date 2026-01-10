be8300382c6d7904310495198ee901794a34056e
Christian Harke <christian@harke.ch>
2026-01-10 16:40:37 +0100

    Improve pdf handling

--- home/roles/desktop/default.nix
+++ home/roles/desktop/default.nix
@@ -77,6 +77,7 @@ in
           "x-scheme-handler/https" = "firefox.desktop";
           "x-scheme-handler/chrome" = "firefox.desktop";
           "text/html" = "firefox.desktop";
+          "application/pdf" = "mupdf.desktop";
           "application/x-extension-htm" = "firefox.desktop";
           "application/x-extension-html" = "firefox.desktop";
           "application/x-extension-shtml" = "firefox.desktop";
