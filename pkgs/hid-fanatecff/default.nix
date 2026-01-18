b1d9cc9c285a36aef60212b55b6c70e2d2a1eee1
Christian Harke <christian@harke.ch>
2026-01-18 09:25:39 +0100

    Implement PR feedback

--- pkgs/hid-fanatecff/default.nix
+++ pkgs/hid-fanatecff/default.nix
@@ -1,42 +1,56 @@
 {
+  lib,
   stdenv,
   fetchFromGitHub,
   kernel,
+  kernelModuleMakeFlags,
+  bashNonInteractive,
   linuxConsoleTools,
+  nix-update-script,
 }:
 
 let
-  moduledir = "lib/modules/${kernel.modDirVersion}/kernel/drivers/hid";
+  moduleDir = "lib/modules/${kernel.modDirVersion}/kernel/drivers/hid";
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "hid-fanatecff";
   version = "0.2.2";
-  name = "hid-fanatecff-${version}-${kernel.modDirVersion}";
 
   src = fetchFromGitHub {
     owner = "gotzl";
     repo = "hid-fanatecff";
-    rev = version;
-    sha256 = "aVuTnrxw7zWMZ1U21DUKDvcYlIp7iHJHaX8ijmUd/TE=";
+    tag = finalAttrs.version;
+    hash = "sha256-aVuTnrxw7zWMZ1U21DUKDvcYlIp7iHJHaX8ijmUd/TE=";
   };
 
-  hardeningDisable = [
-    "pic"
-    "format"
-  ];
   nativeBuildInputs = kernel.moduleBuildDependencies;
 
   patchPhase = ''
-    mkdir -p $out/lib/udev/rules.d
-    mkdir -p $out/${moduledir}
-    substituteInPlace Makefile --replace "/etc/udev/rules.d" "$out/lib/udev/rules.d"
-    substituteInPlace fanatec.rules --replace "/usr/bin/evdev-joystick" "${linuxConsoleTools}/bin/evdev-joystick" --replace "/bin/sh" "${stdenv.shell}"
+    mkdir -p $out/{lib/udev/rules.d,${moduleDir}}
+
     sed -i '/depmod/d' Makefile
+    substituteInPlace Makefile \
+      --replace-fail '/etc/udev/rules.d' "$out/lib/udev/rules.d"
+
+    substituteInPlace fanatec.rules \
+      --replace-fail '/usr/bin/evdev-joystick' '${lib.getExe' linuxConsoleTools "evdev-joystick"}' \
+      --replace-fail '/bin/sh' '${lib.getExe' bashNonInteractive "bash"}'
   '';
 
-  makeFlags = [
+  makeFlags = kernelModuleMakeFlags ++ [
     "KVERSION=${kernel.modDirVersion}"
     "KERNEL_SRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-    "MODULEDIR=$(out)/${moduledir}"
+    "MODULEDIR=$(out)/${moduleDir}"
   ];
-}
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Linux module driver for Fanatec driving wheels";
+    homepage = "https://github.com/gotzl/hid-fanatecff";
+    license = lib.licenses.gpl2Only;
+    maintainers = with lib.maintainers; [ rake5k ];
+    platforms = lib.platforms.linux;
+    mainProgram = "hid-fanatecff";
+  };
+})
