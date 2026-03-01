4f3e166e19582798df2ede8929bbf259c2bd2499
Christian Harke <christian@harke.ch>
2026-03-01 16:45:54 +0100

    Exclude clamav files from backup

--- nixos/base/system/antivirus/default.nix
+++ nixos/base/system/antivirus/default.nix
@@ -1,4 +1,9 @@
 {
+  custom.roles.backup.rsync.jobs.backup.excludes = [
+    "/persist/var/lib/clamav/"
+    "/var/lib/clamav/"
+  ];
+
   services.clamav = {
     daemon.enable = true;
     updater.enable = true;
