ca62abea4add5d5ea8ad30009101fc9b048f4ac7
Christian Harke <christian@harke.ch>
2026-08-10 23:53:50 +0200

    feat(gtk): add code and downloads file manager bookmarks

--- home/roles/desktop/gtk/default.nix
+++ home/roles/desktop/gtk/default.nix
@@ -42,18 +42,24 @@ in
         '';
         force = true;
       };
-      gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-        gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
-        gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
-        gtk-button-images = "1";
-        gtk-menu-images = "1";
-        gtk-enable-event-sounds = "0";
-        gtk-enable-input-feedback-sounds = "0";
-        gtk-xft-antialias = "1";
-        gtk-xft-hinting = "1";
-        gtk-xft-hintstyle = "hintfull";
-        gtk-xft-rgba = "rgb";
+      gtk3 = {
+        bookmarks = [
+          "file://${config.home.homeDirectory}/code Code"
+          "file://${config.home.homeDirectory}/Downloads Downloads"
+        ];
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+          gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
+          gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+          gtk-button-images = "1";
+          gtk-menu-images = "1";
+          gtk-enable-event-sounds = "0";
+          gtk-enable-input-feedback-sounds = "0";
+          gtk-xft-antialias = "1";
+          gtk-xft-hinting = "1";
+          gtk-xft-hintstyle = "hintfull";
+          gtk-xft-rgba = "rgb";
+        };
       };
     };
   };
